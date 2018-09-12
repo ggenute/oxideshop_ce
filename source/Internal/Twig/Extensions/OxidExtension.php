920434643257bd66ac8d3b0226f3dbb6ca58cd3f
Tomasz Kowalewski <t.kowalewski@createit.pl>
2018-09-12 16:34:41 +0200

    OXDEV-1376 Prepare Twig extensions
    Created 4 base Twig extensions and loaded the in TwigEngine constructor

--- source/Internal/Twig/Extensions/OxidExtension.php
+++ source/Internal/Twig/Extensions/OxidExtension.php
@@ -3,7 +3,6 @@
 namespace OxidEsales\EshopCommunity\Internal\Twig\Extensions;
 
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFunction;
 
 /**
  * Class OxidExtension
@@ -15,71 +14,7 @@ class OxidExtension extends AbstractExtension
      */
     public function getFunctions()
     {
-        return [new TwigFunction('oxid_include_widget', [$this, 'oxidIncludeWidget'])];
-
-
-        $sSuffix = isset( $params['suffix'] ) ? $params['suffix'] : 'NO_SUFFIX';
-        $blShowError = isset( $params['noerror']) ? !$params['noerror'] : true ;
-
-        $iLang = $oLang->getTplLanguage();
-
-        if( !$blAdmin && $oShop->isProductiveMode() ) {
-            $blShowError = false;
-        }
-
-        try {
-            $sTranslation = $oLang->translateString( $sIdent, $iLang, $blAdmin );
-            $blTranslationNotFound = !$oLang->isTranslated();
-            if ( 'NO_SUFFIX' != $sSuffix ) {
-                $sSuffixTranslation = $oLang->translateString( $sSuffix, $iLang, $blAdmin );
-            }
-        } catch (\OxidEsales\Eshop\Core\Exception\LanguageException $oEx ) {
-            // is thrown in debug mode and has to be caught here, as smarty hangs otherwise!
-        }
-
-        if( $blTranslationNotFound && isset( $params['alternative'] ) ) {
-            $sTranslation = $params['alternative'];
-            $blTranslationNotFound = false;
-        }
-
-        if ( !$blTranslationNotFound ) {
-            if ( $aArgs !== false ) {
-                if ( is_array( $aArgs ) ) {
-                    $sTranslation = vsprintf( $sTranslation, $aArgs );
-                } else {
-                    $sTranslation = sprintf( $sTranslation, $aArgs );
-                }
-            }
-
-            if ( 'NO_SUFFIX' != $sSuffix ) {
-                $sTranslation .= $sSuffixTranslation;
-            }
-
-        } elseif( $blShowError ) {
-            $sTranslation = 'ERROR: Translation for '.$sIdent.' not found!';
-        }
-
-        stopProfile("smarty_function_oxmultilang");
-
-        return $sTranslation;
+        return [
+        ];
     }
-
-    private function getIncludeWidgetParentViews($params)
-    {
-        $parentViews = null;
-        if(!empty($params["_parent"])) {
-            $parentViews = explode("|", $params["_parent"]);
-        }
-        return $parentViews;
-    }
-
-    private function getIncludeWidgetFormattedParams($params)
-    {
-        unset($params['cl']);
-        if(!empty($params["_parent"])) {
-            unset($params["_parent"]);
-        }
-        return $params;
-    }
-
 }
