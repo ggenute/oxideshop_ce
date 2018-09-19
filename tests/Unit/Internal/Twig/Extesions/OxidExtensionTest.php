58087be8c59125735c854ab5125515e93166bdc4
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-09-19 13:39:46 +0200

    OXDEV-1376 Fix isse with OxidExtensionTest
    It seems that incorrect class was extended.

--- tests/Unit/Internal/Twig/Extesions/OxidExtensionTest.php
+++ tests/Unit/Internal/Twig/Extesions/OxidExtensionTest.php
@@ -11,7 +11,7 @@ namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Twig\Extesions;
 use OxidEsales\EshopCommunity\Internal\Twig\Extensions\OxidExtension;
 use OxidEsales\EshopCommunity\Core\Registry;
 
-class OxidExtensionTest extends \PHPUnit_Framework_TestCase
+class OxidExtensionTest extends \PHPUnit\Framework\TestCase
 {
 
     private function getMethod(OxidExtension $oxidExtension, $name) {
