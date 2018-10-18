d1674d802e8e477ed55035e2e0391f5a241b5295
Mantas Vaitkunas <mantas.vaitkunas@oxid-esales.com>
2018-10-18 17:29:14 +0300

    OXDEV-1580 Fix shop switch issue
    Shop was not switched because of UtilsObject static instance. Now before
    bootstrapping shop, shop-id is being set as request parameter.

---
+++ source/Internal/Console/ShopBootstrapper.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Console;
+
+use OxidEsales\Facts\Facts;
+use Webmozart\PathUtil\Path;
+
+/**
+ * Bootstraps OXID eShop by specified shop id.
+ */
+class ShopBootstrapper
+{
+    /**
+     * @param int $shopId
+     */
+    public function bootstrap(int $shopId)
+    {
+        $_POST['shp'] = $shopId;
+        $bootstrapFilePath = Path::join((new Facts())->getSourcePath(), 'bootstrap.php');
+        require_once $bootstrapFilePath;
+    }
+}
