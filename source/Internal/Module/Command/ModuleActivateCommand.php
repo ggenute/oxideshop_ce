cc49e5a18b016139653384fcc38fe9551e3c0b87
Mantas Vaitkunas <mantas.vaitkunas@oxid-esales.com>
2018-10-24 12:11:36 +0300

    OXDEV-1580 Refactor module activation command

--- source/Internal/Module/Command/ModuleActivateCommand.php
+++ source/Internal/Module/Command/ModuleActivateCommand.php
@@ -8,14 +8,15 @@ namespace OxidEsales\EshopCommunity\Internal\Module\Command;
 
 use OxidEsales\Eshop\Core\Module\Module;
 use OxidEsales\Eshop\Core\Module\ModuleInstaller;
-use OxidEsales\Eshop\Core\Module\ModuleList;
 use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\EshopCommunity\Internal\Adapter\ShopAdapterInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
+ * Command activates module by module id.
  * @internal
  */
 class ModuleActivateCommand extends Command
@@ -26,39 +27,64 @@ class ModuleActivateCommand extends Command
 
     const MESSAGE_MODULE_NOT_FOUND = 'Module - "%s" not found.';
 
+
+    /**
+     * @var ShopAdapterInterface
+     */
+    private $shopAdapter;
+
     /**
      * @inheritdoc
      */
-    protected function configure()
+    public function __construct($name = null, ShopAdapterInterface $shopAdapter)
     {
-        $this->setName('oe:module:activate')
-            ->setDescription('Activates a module')
-            ->addArgument('module-id', InputArgument::REQUIRED, 'Module ID')
-            ->setHelp('Command activates module by defined module ID.');
+        parent::__construct($name);
+        $this->shopAdapter = $shopAdapter;
     }
 
     /**
      * @inheritdoc
      */
+    protected function configure()
+    {
+        $this->setDescription('Activates a module.')
+            ->addArgument('module-id', InputArgument::REQUIRED, 'Module ID')
+            ->setHelp('Command activates module by defined module ID.');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @throws \OxidEsales\Eshop\Core\Exception\StandardException
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $moduleId = $input->getArgument('module-id');
-        /** @var ModuleInstaller $moduleInstaller */
-        $moduleInstaller = Registry::get(ModuleInstaller::class);
-        $moduleList = oxNew(ModuleList::class);
-        $moduleList->getModulesFromDir(Registry::getConfig()->getModulesDir());
-        $modules = $moduleList->getList();
+        $modules = $this->shopAdapter->getModules();
+
         /** @var Module $module */
         if (isset($modules[$moduleId])) {
-            $module = $modules[$moduleId];
-            if ($module->isActive()) {
-                $output->writeLn('<info>'.sprintf(static::MESSAGE_MODULE_ALREADY_ACTIVE, $moduleId).'</info>');
-            } else {
-                $moduleInstaller->activate($module);
-                $output->writeLn('<info>'.sprintf(static::MESSAGE_MODULE_ACTIVATED, $moduleId).'</info>');
-            }
+            $this->activateModule($output, $modules[$moduleId]);
         } else {
             $output->writeLn('<error>'.sprintf(static::MESSAGE_MODULE_NOT_FOUND, $moduleId).'</error>');
+        }
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param Module          $module
+     * @throws \OxidEsales\Eshop\Core\Exception\StandardException
+     */
+    protected function activateModule(OutputInterface $output, Module $module)
+    {
+        /** @var ModuleInstaller $moduleInstaller */
+        $moduleInstaller = Registry::get(ModuleInstaller::class);
+
+        if ($module->isActive()) {
+            $output->writeLn('<info>'.sprintf(static::MESSAGE_MODULE_ALREADY_ACTIVE, $module->getId()).'</info>');
+        } else {
+            $moduleInstaller->activate($module);
+            $output->writeLn('<info>'.sprintf(static::MESSAGE_MODULE_ACTIVATED, $module->getId()).'</info>');
         }
     }
 }
--- tests/Integration/Internal/Module/Command/ModuleActivateCommandTest.php
+++ tests/Integration/Internal/Module/Command/ModuleActivateCommandTest.php
@@ -7,21 +7,11 @@
 namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Module\Command;
 
 use OxidEsales\Eshop\Core\Module\Module;
-use OxidEsales\Eshop\Core\Module\ModuleList;
-use OxidEsales\EshopCommunity\Core\Registry;
 use OxidEsales\EshopCommunity\Internal\Module\Command\ModuleActivateCommand;
-use OxidEsales\EshopCommunity\Tests\Integration\Internal\Console\ConsoleTrait;
-use OxidEsales\EshopCommunity\Tests\Integration\Internal\ContainerTrait;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Filesystem\Filesystem;
 
-class ModuleActivateCommandTest extends TestCase
+class ModuleActivateCommandTest extends ModuleCommandsTestCase
 {
-    use ContainerTrait;
-    use ConsoleTrait;
-
     public function testModuleActivation()
     {
         $this->prepareTestData();
@@ -42,9 +32,6 @@ class ModuleActivateCommandTest extends TestCase
         $this->cleanupTestData();
     }
 
-    /**
-     * @depends testModuleActivation
-     */
     public function testWhenModuleAlreadyActive()
     {
         $this->prepareTestData();
@@ -71,30 +58,5 @@ class ModuleActivateCommandTest extends TestCase
         );
 
         $this->assertSame(sprintf(ModuleActivateCommand::MESSAGE_MODULE_NOT_FOUND, $moduleId) . PHP_EOL, $consoleOutput);
-    }
-
-    protected function cleanupTestData()
-    {
-        $fileSystem = new Filesystem();
-        $fileSystem->remove(Registry::getConfig()->getModulesDir() . '/testmodule');
-        $moduleList = oxNew(ModuleList::class);
-        $moduleList->cleanup();
-    }
-
-    protected function prepareTestData()
-    {
-        $fileSystem = new Filesystem();
-        $fileSystem->mirror(__DIR__ . '/Fixtures', Registry::getConfig()->getConfigParam('sShopDir'));
-    }
-
-    /**
-     * @return Application
-     */
-    private function getApplication(): Application
-    {
-        $application = $this->get('oxid_esales.console.symfony.component.console.application');
-        $application->setAutoExit(false);
-
-        return $application;
     }
 }
